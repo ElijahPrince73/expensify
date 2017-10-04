3ca6477f644bb2b7dcfe5c096c25b595cda4600e
elijahprince73 <elijahprince73@gmail.com>
2017-10-04 14:57:12 -0700

    added destructoring to redux-101.js

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,19 +2,42 @@ import {
   createStore
 } from 'redux';
 
+const incrementCount = ({ incrementBy = 1 } = {}) => {
+  return {
+    type: 'INCREMENT',
+    incrementBy: incrementBy
+  }
+}
+
+const decrementCount = ({ decrementBy = 1} = {}) => ({
+  type: 'DECREMENT',
+  decrementBy: decrementBy
+})
+
+const setCount = ({ count = 0} = {}) => ({
+  type:'SET',
+  count: count
+})
+
+const resetCount = () => ({
+  type: 'RESET'
+})
+
 const store = createStore((state = {
   count: 0
 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
+      };
+      case 'SET':
+      return {
+        count: action.count
       };
     case 'RESET':
       return {
@@ -29,17 +52,16 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-})
-store.dispatch({
-  type: 'INCREMENT',
-})
-store.dispatch({
-  type: 'RESET'
-})
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10
-})
+store.dispatch(setCount({ count: 100}))
+
+store.dispatch(incrementCount({
+  incrementBy: 6
+}))
+store.dispatch(incrementCount())
+
+store.dispatch(decrementCount())
+store.dispatch(decrementCount({
+  decrementBy:10
+}))
+
+store.dispatch(resetCount())
