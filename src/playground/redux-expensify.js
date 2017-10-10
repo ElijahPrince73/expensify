78f83998585e3eb01cee09f2470158dc4986b5b3
Elijah Prince <elijahprince73@gmail.com>
2017-10-09 22:12:41 -0700

    filtering redux data

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -106,13 +106,26 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
 // Store creation
 const store = createStore(combineReducers({expenses: expensesReducer, filters: filtersReducer}));
 
+//Get visible expenses
+const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
+  return expenses.filter((expense) => {
+    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch
+  });
+
+}
+
 store.subscribe(() => {
-  console.log(store.getState());
-  console.log('break');
+  const state = store.getState()
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+  console.log(visibleExpenses);
 });
 
-// const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100}));
-// const expenseTwo = store.dispatch(addExpense({description: 'Coffee', amount: 300}));
+const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100, createdAt: 1000}));
+const expenseTwo = store.dispatch(addExpense({description: 'Coffee', amount: 300, createdAt: -1000}));
 //
 // store.dispatch(removeExpense({id: expenseOne.expense.id}));
 //
@@ -124,11 +137,12 @@ store.subscribe(() => {
 // store.dispatch(sortByAmount())
 // store.dispatch(sortByDate())
 
-store.dispatch(setStartDate(125))
-store.dispatch(setStartDate())
-
+store.dispatch(setStartDate(0))
+// store.dispatch(setStartDate())
+//
 store.dispatch(setEndDate(125))
-store.dispatch(setEndDate())
+// store.dispatch(setEndDate())
+
 const demoState = {
   expenses: [
     {
